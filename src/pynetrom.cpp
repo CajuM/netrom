88717d690b0313462e0d200902b797d8fbfcdf46
Mihai Drosi Caju <mihai-drosi.caju@fmi.unibuc.ro>
2015-12-24 17:10:11 +0200

    some more subtle changes, at least it builds

--- src/pynetrom.cpp
+++ src/pynetrom.cpp
@@ -25,12 +25,4 @@ BOOST_PYTHON_MODULE(pynetrom)
 			py::make_constructor(&do_nothing));
 
 	class_<netrom::GameObject>("GameObject");
-
-	class_<netrom::GameObjectList>("GameObjectList")
-			.def("__delitme__", &netrom::GameObjectList::__delitem__)
-			.def("__getitme__", &netrom::GameObjectList::__getitem__,
-					return_value_policy<reference_existing_object>()
-			).def("__setitme__", &netrom::GameObjectList::__setitem__)
-			.def("__len__", &netrom::GameObjectList::__len__)
-			.def("append", &netrom::GameObjectList::append);
 }
